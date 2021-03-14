505646c12369b041d24bb70b0c20b39e0762e496
tesser4 <kostas@tux.gr>
2021-03-14 22:08:23 +0200

    Centralize remote dependencies in deps file

--- linked-list.test.js
+++ linked-list.test.js
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts'
+import { assertEquals } from './deps.js'
 import getLinkedList from './linked-list.js'
 
 const ll = getLinkedList()
